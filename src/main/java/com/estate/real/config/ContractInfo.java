6091d2de9d5d38ef22318ff8aed29c9bfcc4af4c
Pham Hau <hauphvn@gmail.com>
2020-06-25 23:52:59 +0700

    + Deploy smart contract

---
+++ src/main/java/com/estate/real/config/ContractInfo.java
@@ -0,0 +1,7 @@
+package com.estate.real.config;
+
+public class ContractInfo {
+    public static final String pkDeploy = "2042B83D51170D3839D4DEACA9DACFCFE574EB032D8716590AC8044F446417BC";
+    public static final String locationEthereum = "HTTP://127.0.0.1:7545";
+    public static final String addressContract = "0xf930f0cb651e158693d50c7f5db9c12acd845923";
+}
--- src/main/java/com/estate/real/service/impl/democontract.java
+++ src/main/java/com/estate/real/service/impl/democontract.java
@@ -1,13 +1,42 @@
 package com.estate.real.service.impl;
 
+import com.estate.real.config.ContractInfo;
+import com.estate.real.contract.ManageRealEsate;
+import org.springframework.http.server.reactive.JettyHttpHandlerAdapter;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.DefaultGasProvider;
+import org.web3j.utils.Convert;
+
+import java.math.BigInteger;
 
 public class democontract {
-    Web3j web3 = Web3j.build(new HttpService("HTTP://127.0.0.1:7545"));
-    String pk = "2042B83D51170D3839D4DEACA9DACFCFE574EB032D8716590AC8044F446417BC";
-    Credentials credentials = Credentials.create(pk);
+    public static void main(String[] args) {
+        Web3j web3j = Web3j.build(new HttpService(ContractInfo.locationEthereum));
+        Credentials credentials = Credentials.create(ContractInfo.pkDeploy);
+        //3.Configure gas parameters
+        BigInteger gasLimit = BigInteger.valueOf(672197885);
+
+        BigInteger gasPrice = Convert.toWei("0.0000000000000000000000000001", Convert.Unit.GWEI).toBigInteger();
+        try {
+//            ManageRealEsate manageRealEsate = ManageRealEsate.deploy(web3j, credentials, gasPrice, gasLimit).send();
+//            System.out.println("Smart contract address: "+manageRealEsate.getContractAddress());
+
+            ManageRealEsate manageRealEsate = ManageRealEsate.load(ContractInfo.addressContract, web3j, credentials,
+                    gasLimit, gasPrice);
+
+            TransactionReceipt transactionReceipt = manageRealEsate.addLand("Quan 1", "Tran dudf",
+                    BigInteger.valueOf(1000000)).send();
+            System.out.println("add status: " + transactionReceipt.isStatusOK());
+        } catch (Exception e) {
+//            System.out.println("Loi deploy smart contract");
+            e.printStackTrace(System.out);
+        }
+    }
+
+
 //        try
 //
 //    {
@@ -25,22 +54,22 @@ public class democontract {
 //            System.out.println("Gas price: "+gasPrice.getGasPrice());
 //            System.out.println("Block number: "+blockNumber.getBlockNumber());
 
-        //Load a wallet
+    //Load a wallet
 
-        //Send funds from one account to another
-        //1. Get none:
+    //Send funds from one account to another
+    //1. Get none:
 //        EthGetTransactionCount getTransactionCount = web3.ethGetTransactionCount(credentials.getAddress(), DefaultBlockParameterName.LATEST).send();
 //        BigInteger nonce = getTransactionCount.getTransactionCount();
 //        System.out.println("nonce: "+nonce);
-        //2.Configure recipient account and amount to send
-        String recipientAddress = "0x89E8416eA5b69863CCb34D3A5C74A86bf5549147";
+    //2.Configure recipient account and amount to send
+//        String recipientAddress = "0x89E8416eA5b69863CCb34D3A5C74A86bf5549147";
 //        BigInteger valueToTranfer = Convert.toWei("1", Convert.Unit.ETHER).toBigInteger();
 //        System.out.println(valueToTranfer);
-        //3.Configure gas parameters
+    //3.Configure gas parameters
 //        BigInteger gasLimit = BigInteger.valueOf(21000);
 //        BigInteger gasPrice = Convert.toWei("1", Convert.Unit.GWEI).toBigInteger();
 //        System.out.println("gasPrice: " + gasPrice);
-        //Prepare a rew transaction
+    //Prepare a rew transaction
 //        RawTransaction rawTransaction = RawTransaction.createEtherTransaction(
 //                nonce,
 //                gasPrice,
@@ -48,16 +77,16 @@ public class democontract {
 //                recipientAddress,
 //                valueToTranfer
 //        );
-        //Sign the transaction
+    //Sign the transaction
 //        byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
 //        System.out.println("byte[] signedMessage: " + signedMessage.toString());
 //        //Convert it to Hexadecimal String to be send to the node
 //        String hexValue  = Numeric.toHexString(signedMessage);
 //        System.out.println("hexValue: "+ hexValue);
 
-        //Send to the node via JSON RPC.
+    //Send to the node via JSON RPC.
 //        EthSendTransaction ethSendTransaction= web3.ethSendRawTransaction(hexValue).send();
-        //Get the transaction hash
+    //Get the transaction hash
 //        String transactionHash = ethSendTransaction.getTransactionHash();
 //        System.out.println("Transaction hash: " + transactionHash);
 // Wait for transaction to be mined
