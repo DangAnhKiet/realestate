ef120edd057368734b2e8a5f70c9d747bbbd5aa3
PHAM VAN HAU <hauphvn@gmail.com>
2020-07-04 00:35:15 +0700

    my message

--- src/main/java/com/estate/real/config/ContractInfo.java
+++ src/main/java/com/estate/real/config/ContractInfo.java
@@ -3,5 +3,5 @@ package com.estate.real.config;
 public class ContractInfo {
     public static final String pkDeploy = "494112cab9ea77233e3259d5a016a265aae9a8006d1e94c109823962ec7df677";
     public static final String locationEthereum = "HTTP://127.0.0.1:7545";
-    public static final String addressContract = "0x6edac5e2c432b55fd1fb41962075ed12dbf42fc4";
+    public static final String addressContract = "0x4ad56c1d992d579aeff9b27e49b207aa9e28e649";
 }
