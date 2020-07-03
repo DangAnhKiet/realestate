933a2858ab3bb4bedb069f76fa7da86be44dad35
PHAM VAN HAU <hauphvn@gmail.com>
2020-07-04 01:21:59 +0700

    fix gitignore

--- src/main/java/com/estate/real/config/ContractInfo.java
+++ src/main/java/com/estate/real/config/ContractInfo.java
@@ -4,4 +4,5 @@ public class ContractInfo {
     public static final String pkDeploy = "494112cab9ea77233e3259d5a016a265aae9a8006d1e94c109823962ec7df677";
     public static final String locationEthereum = "HTTP://127.0.0.1:7545";
     public static final String addressContract = "0x89E8416eA5b69863CCb34D3A5C74A86bf5549147";
+
 }
